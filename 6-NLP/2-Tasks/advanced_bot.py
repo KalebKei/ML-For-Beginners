7ad4b30ae1125ffccc2a905d2c474b6d234b3e33
KalebKei <kk471921@ohio.edu>
2023-11-11 15:54:18 -0500

    Work in progress on 6-NLP. Enjoying the process, so I want to make the bot special.

---
+++ 6-NLP/2-Tasks/advanced_bot.py
@@ -0,0 +1,38 @@
+# More improved bot with the ability to "relate" more.
+import random
+from textblob import TextBlob
+
+random_responses = ["That is quite interesting, please tell me more.",
+                    "I see. Do go on.",
+                    "Why do you say that?",
+                    "Funny weather we've been having, isn't it?",
+                    "Let's change the subject.",
+                    "Did you catch the game last night?"]
+
+print("Hello, I am a simple conversational bot. I am tasked with conversing with you, would you like to join me?\n")
+print("To converse, please wait until I respond, then simply type into the command line. Finish your thought by pressing enter. To finish our conversation, please request to \"bye\".\n")
+
+print("How are you doing today?")
+
+userInput = input()
+while(userInput.lower() != "bye"): 
+    user_input_blob = TextBlob(userInput).correct()
+    if user_input_blob.sentiment.polarity <= -0.5:
+        response = "Oh dear, that sounds bad. "
+    elif user_input_blob.sentiment.polarity < 0:
+        response = "Hmm, that's not great. "
+    elif user_input_blob.sentiment.polarity <= 0.5:
+        response = "Well, that sounds good. "
+    elif user_input_blob.sentiment.polarity <= 1:
+        response = "Wow, that sounds great. "
+    print(response)
+
+    print("*********************")
+    print(user_input_blob)
+    print(user_input_blob.sentiment)
+    print(user_input_blob.sentiment.polarity)
+    print(user_input_blob.noun_phrases)
+    print("*********************")
+    userInput = input()
+
+print("Have a great rest of your day!")
